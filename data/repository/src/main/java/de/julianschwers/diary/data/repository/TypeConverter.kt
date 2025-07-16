e831941c2da5ffbff7c03de40373b3b02874e7b9
Julian Schwers <juli.schwers@gmail.com>
2025-07-17 01:20:13 +0200

    Added time to TypeConverter

--- data/repository/src/main/java/de/julianschwers/diary/data/repository/TypeConverter.kt
+++ data/repository/src/main/java/de/julianschwers/diary/data/repository/TypeConverter.kt
@@ -10,6 +10,7 @@ internal fun JournalEntryData.asModel(): JournalEntry =
     JournalEntry(
         text = text ?: "",
         moodUid = moodUid,
+        time = Instant.fromEpochMilliseconds(timeMillis),
         
         created = Instant.fromEpochMilliseconds(createdMillis),
         uid = uid
@@ -20,6 +21,7 @@ internal fun JournalEntry.asData(storedData: JournalEntryData): JournalEntryData
     storedData.apply {
         this@apply.text = this@asData.text.takeUnless { it.isBlank() }
         this@apply.moodUid = this@asData.moodUid
+        this@apply.timeMillis = this@asData.time.toEpochMilliseconds()
         
         this@apply.createdMillis = this@asData.created.toEpochMilliseconds()
         this@apply.uid = this@asData.uid
