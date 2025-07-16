ddf16a30d7995f35b581c77443621adeb70d5812
Julian Schwers <juli.schwers@gmail.com>
2025-07-17 00:39:07 +0200

    Added Room DB implementation

---
+++ data/database/src/main/java/de/julianschwers/diary/data/database/room/RoomJournalEntry.kt
@@ -0,0 +1,17 @@
+package de.julianschwers.diary.data.database.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import de.julianschwers.diary.data.database.JournalEntryData
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
+
+@Entity
+data class RoomJournalEntry @OptIn(ExperimentalTime::class) constructor(
+    override var text: String? = null,
+    override var moodUid: String? = null,
+    override var timeMillis: Long = Clock.System.now().toEpochMilliseconds(),
+    
+    override var createdMillis: Long = Clock.System.now().toEpochMilliseconds(),
+    @PrimaryKey override val uid: String,
+) : JournalEntryData
