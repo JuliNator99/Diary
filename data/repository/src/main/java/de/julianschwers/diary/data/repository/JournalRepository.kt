e6c4671a8644f91067ed137e59c9a28c1ed9be9a
Julian Schwers <juli.schwers@gmail.com>
2025-07-17 01:15:49 +0200

    Added JournalRepository

---
+++ data/repository/src/main/java/de/julianschwers/diary/data/repository/JournalRepository.kt
@@ -0,0 +1,15 @@
+package de.julianschwers.diary.data.repository
+
+import de.julianschwers.diary.core.model.JournalEntry
+import de.julianschwers.diary.data.database.Database
+import de.julianschwers.diary.data.database.copyImpl
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class JournalRepository(private val database: Database) {
+    fun getJournal(uid: String): JournalEntry = database.journalEntryDao.getJournal(uid = uid).asModel()
+    fun queryJournals(): Flow<List<JournalEntry>> = database.journalEntryDao.queryJournals().map { flow -> flow.map { it.asModel() } }
+    
+    fun upsert(journal: JournalEntry) = database.journalEntryDao.upsert(journal = journal.asData(database.journalEntryDao.getJournal(uid = journal.uid).copyImpl()))
+    fun delete(journal: JournalEntry) = database.journalEntryDao.delete(journal = journal.asData(database.journalEntryDao.getJournal(uid = journal.uid).copyImpl()))
+}
