05336623025a7e202217a32c01f0ac9fa2555b86
Julian Schwers <juli.schwers@gmail.com>
2025-07-20 18:22:31 +0200

    Added new Mood enum

--- core/model/src/main/java/de/julianschwers/diary/core/model/MoodData.kt
+++ core/model/src/main/java/de/julianschwers/diary/core/model/MoodData.kt
@@ -3,6 +3,14 @@ package de.julianschwers.diary.core.model
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
+enum class Mood {
+    VERY_LOW,
+    LOW,
+    MODERATE,
+    HIGH,
+    VERY_HIGH
+}
+
 data class MoodData @OptIn(ExperimentalUuidApi::class) constructor(
     val emoji: String = "",
     
