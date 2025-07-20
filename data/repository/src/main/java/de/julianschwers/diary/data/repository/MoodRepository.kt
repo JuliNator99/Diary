d4ecb8e6ec9cf3f4713bed02e596fe5b3f46b8bd
Julian Schwers <juli.schwers@gmail.com>
2025-07-20 18:56:29 +0200

    Added names to Moods

--- data/repository/src/main/java/de/julianschwers/diary/data/repository/MoodRepository.kt
+++ data/repository/src/main/java/de/julianschwers/diary/data/repository/MoodRepository.kt
@@ -1,14 +1,24 @@
 package de.julianschwers.diary.data.repository
 
+import androidx.annotation.StringRes
 import de.julianschwers.diary.core.model.Mood
 
 class MoodRepository {
-    fun getEmoji(mood: Mood): String =
-        when (mood) {
-            Mood.VERY_LOW  -> "D:"
-            Mood.LOW       -> ":("
-            Mood.MODERATE  -> ":|"
-            Mood.HIGH      -> ":)"
-            Mood.VERY_HIGH -> ":D"
-        }
+    @StringRes
+    fun getName(mood: Mood): Int = when (mood) {
+        Mood.VERY_LOW  -> R.string.mood_name_verylow
+        Mood.LOW       -> R.string.mood_name_low
+        Mood.MODERATE  -> R.string.mood_name_moderate
+        Mood.HIGH      -> R.string.mood_name_high
+        Mood.VERY_HIGH -> R.string.mood_name_veryhigh
+    }
+    
+    fun getEmoji(mood: Mood): String = when (mood) {
+        Mood.VERY_LOW  -> "D:"
+        Mood.LOW       -> ":("
+        Mood.MODERATE  -> ":|"
+        Mood.HIGH      -> ":)"
+        Mood.VERY_HIGH -> ":D"
+    }
+    
 }
