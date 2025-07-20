2171d8e0b3f9d07823bbcd0a6aa12839a5e43c32
Julian Schwers <juli.schwers@gmail.com>
2025-07-20 19:15:16 +0200

    Added simple color palette for Moods

--- data/repository/src/main/java/de/julianschwers/diary/data/repository/MoodRepository.kt
+++ data/repository/src/main/java/de/julianschwers/diary/data/repository/MoodRepository.kt
@@ -1,9 +1,18 @@
 package de.julianschwers.diary.data.repository
 
+import androidx.annotation.ColorRes
 import androidx.annotation.StringRes
 import de.julianschwers.diary.core.model.Mood
 
 class MoodRepository {
+    @ColorRes
+    fun getColor(mood: Mood): Int = when (mood) {
+        Mood.VERY_LOW  -> R.color.mood_color_verylow
+        Mood.LOW       -> R.color.mood_color_low
+        Mood.MODERATE  -> R.color.mood_color_moderate
+        Mood.HIGH      -> R.color.mood_color_high
+        Mood.VERY_HIGH -> R.color.mood_color_veryhigh
+    }
     @StringRes
     fun getName(mood: Mood): Int = when (mood) {
         Mood.VERY_LOW  -> R.string.mood_name_verylow
