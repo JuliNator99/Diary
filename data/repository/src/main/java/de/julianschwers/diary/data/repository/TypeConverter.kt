b1361154d894be97c7ae9923cfb39220dee6fd10
Julian Schwers <juli.schwers@gmail.com>
2025-07-29 13:29:07 +0200

    Added attachments data to JournalEntry Model

--- data/repository/src/main/java/de/julianschwers/diary/data/repository/TypeConverter.kt
+++ data/repository/src/main/java/de/julianschwers/diary/data/repository/TypeConverter.kt
@@ -12,6 +12,7 @@ internal fun JournalEntryData.asModel(): JournalEntry =
         text = text ?: "",
         mood = moodUid?.let { runCatching { Mood.valueOf(it) }.getOrNull() },
         time = Instant.fromEpochMilliseconds(timeMillis),
+        attachments = attachmentsList?.split("//") ?: emptyList(),
         
         created = Instant.fromEpochMilliseconds(createdMillis),
         uid = uid
@@ -23,6 +24,7 @@ internal fun JournalEntry.asData(storedData: JournalEntryData): JournalEntryData
         this@apply.text = this@asData.text.takeUnless { it.isBlank() }
         this@apply.moodUid = this@asData.mood?.name
         this@apply.timeMillis = this@asData.time.toEpochMilliseconds()
+        this@apply.attachmentsList = this@asData.attachments.let { attachments -> if (attachments.isEmpty()) null else attachments.joinToString("//") }
         
         this@apply.createdMillis = this@asData.created.toEpochMilliseconds()
         this@apply.uid = this@asData.uid
